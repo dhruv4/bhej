c482bc3bd43c1fe5c9b8b1ba48b8d4929a668376
ivraj <iseerha@gmail.com>
2020-10-29 13:45:54 -0500

    Adds catch for errors in cli

--- cli/main.py
+++ cli/main.py
@@ -1,10 +1,26 @@
+import config 
+import requests
 import typer
 
 app = typer.Typer()
+url = config.API_URL
 
 @app.command()
 def up(filename: str):
+    try: 
+        files = {'upload_file': open(filename, 'rb')}
+    except FileNotFoundError as err:
+        typer.echo(f"No such file: '{filename}'. Aborting.")
+        return
+    except Exception as err:
+        typer.echo(f"Unexpected Error: {str(err)}")
+        raise(err)
+    
     typer.echo(f"Uploading {filename}")
+
+    req = requests.post(url + '/upload', files=files)
+    return req.text
+    
 
 @app.command()
 def down(filecode: str):
