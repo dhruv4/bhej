aa6cf6aaf4b6b6baf96aa53162ce9fcf6aeb4888
ivraj <iseerha@gmail.com>
2020-10-29 12:49:43 -0500

    Addes req.txt to cli

--- cli/main.py
+++ cli/main.py
@@ -3,15 +3,12 @@ import typer
 app = typer.Typer()
 
 @app.command()
-def hello(name: str):
-    typer.echo(f"Hello {name}")
+def up(filename: str):
+    typer.echo(f"Uploading {filename}")
 
 @app.command()
-def goodbye(name: str, formal: bool = False):
-    if formal:
-        typer.echo(f"Goodbye Ms. {name}. Have a good day.")
-    else:
-        typer.echo(f"Bye {name}!")
+def down(filecode: str):
+    typer.echo(f"Downloading {filecode}")
 
 
 if __name__ == "__main__":
