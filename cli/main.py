5d6b35fbf7f1052929a19bef8fd01613b6f5759f
ivraj <iseerha@gmail.com>
2020-10-29 16:31:00 -0500

    Adds dest arg to cli

--- cli/main.py
+++ cli/main.py
@@ -3,6 +3,7 @@ import config
 import requests
 import magic
 import typer
+import os
 
 app = typer.Typer()
 url = config.API_URL
@@ -29,11 +30,26 @@ def up(filename: str):
     
 
 @app.command()
-def down(filecode: str):
+def down(filecode: str, dest: str = '.'):
+    # TODO: Add an option to change the file name? 
+
+    if not os.path.exists(dest):
+        typer.echo(f"No such location: {dest}. Aborting.")
+        return
+
+    if not os.path.isdir(dest):
+        typer.echo(f"{dest} is not a directory. Aborting.")
+        # TODO: Add a prompt to ask whether you'd like to create the dir.
+        return
+
+    dest = os.path.join(dest, '') # Adds trailing slash if missing.
+
     typer.echo(f"Downloading {filecode}")
     req = requests.get(url + '/file/'+ filecode)
-    value, params = cgi.parse_header(req.headers['Content-Disposition'])
-    open(params['filename'], 'wb').write(req.content)
+    _, params = cgi.parse_header(req.headers['Content-Disposition'])
+
+    open(dest + params['filename'], 'wb').write(req.content)
+    typer.echo("Done!")
 
 if __name__ == "__main__":
     app()
